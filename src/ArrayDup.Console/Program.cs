0f67391f562459c0c9b1127c4bf74e0257a6e8cc
DwarfPorter <mrvlmor@gmail.com>
2021-11-21 21:39:35 +0300

    Основная структура приложения

---
+++ src/ArrayDup.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ArrayDup.Console
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
