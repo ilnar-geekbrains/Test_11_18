09cb21a6a7d7fa764cb48c1c14ec43476e310f99
Vlad <55292673+LeatherDeerAU@users.noreply.github.com>
2021-11-25 23:09:51 +0300

    added some tests

--- Solution.java
+++ Solution.java
@@ -22,4 +22,34 @@ class Solution {
         }
         return result;
     }
+    
+    public static void main(String[] args) {
+        int[] a = new int[]{-1, 1};
+        int[] b = new int[]{2, 1};
+        int[] result = polynomMulti(a, b);
+        if (result[0] == -2 && result[1] == 1 && result[2] == 1) {
+            System.out.println("Тест 1 пройден");
+        } else {
+            System.out.println("Тест 1 не пройден");
+        }
+
+        int[] source1 = new int[]{};
+        int[] source2 = new int[]{1};
+        int[] result1 = copy(source1);
+        int[] result2 = copy(source2);
+
+        if (result1 != source1 && result1.length == 0) {
+            System.out.println("Тест 2 пройден");
+        } else {
+            System.out.println("Тест 2 не пройден");
+        }
+
+        if (result2 != source2 && result2[0] == source2[0]) {
+            System.out.println("Тест 3 пройден");
+        } else {
+            System.out.println("Тест 3 не пройден");
+        }
+
+
+    }
 }
