9f6cac41651a3d9dc95177259dfee91b9df29e12
Ivan Cheklin <ivan.cheklin.1985@gmail.com>
2022-04-09 22:48:04 +0300

    modify: task_2.py - added testing system

--- task_2.py
+++ task_2.py
@@ -1,3 +1,5 @@
+from itertools import combinations_with_replacement
+
 def product_of_polynom(poly_1, poly_2):
     result = []
     for (index1, coef1) in enumerate(poly_1):
@@ -12,4 +14,14 @@ def product_of_polynom(poly_1, poly_2):
 
 
 if __name__ == "__main__":
-    assert product_of_polynom([-1, 1], [2, 1]) == [-2, 1, 1]
+    data = [
+            [[-1, 1], [2, 1], [-2, 1, 1]],
+            ]
+    for poly1, poly2, result in data:
+        try:
+            assert product_of_polynom(poly1, poly2) == result
+        except AssertionError:
+            print('Test failed')
+            print(f'Returns {product_of_polynom(poly1, poly2)}, but must be {result}')
+        finally:
+            print('All tests is OK')
