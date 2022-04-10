96565d3ce2cc29caa6ea74e2350e34f7c56904ec
Ivan Cheklin <ivan.cheklin.1985@gmail.com>
2022-04-10 15:18:51 +0300

    modify: added tests

--- task_2.py
+++ task_2.py
@@ -1,6 +1,11 @@
 from itertools import combinations_with_replacement
 
 def product_of_polynom(poly_1, poly_2):
+    if not poly_1:
+        poly_1 = [0]
+    if not poly_2:
+        poly_2 = [0]
+
     result = []
     for (index1, coef1) in enumerate(poly_1):
         for (index2, coef2) in enumerate(poly_2):
@@ -10,18 +15,24 @@ def product_of_polynom(poly_1, poly_2):
                 result.append(coef)
             else:
                 result[index] += coef
+    for i in range(len(result) - 1):
+        if result[-1] == 0:
+            result.pop(-1)
     return result
 
 
 if __name__ == "__main__":
     data = [
             [[-1, 1], [2, 1], [-2, 1, 1]],
+            [[2, 3, 1], [1, -2, 0, -1], [2, -1, -5, -4, -3, -1]],
+            [[0], [1, 2, 3, 4, 5], [0]],
+            [[], [], [0]],
+            [[], [-4, -2, -8], [0]]
             ]
-    for poly1, poly2, result in data:
-        try:
+    try:
+        for poly1, poly2, result in data:
             assert product_of_polynom(poly1, poly2) == result
-        except AssertionError:
-            print('Test failed')
-            print(f'Returns {product_of_polynom(poly1, poly2)}, but must be {result}')
-        finally:
-            print('All tests is OK')
+        print('All tests is OK')
+    except AssertionError:
+        print('Test failed')
+        print(f'Returns {product_of_polynom(poly1, poly2)}, but must be {result}')
