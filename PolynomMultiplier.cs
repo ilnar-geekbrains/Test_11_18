ce9bd441e21364ad58dff5f4f112972a4919b8a2
hunm <paulcky@yandex.ru>
2022-06-20 14:54:41 +0500

    TASK-2: fix bags and add tests

--- PolynomMultiplier.cs
+++ PolynomMultiplier.cs
@@ -1,25 +1,111 @@
 using System;
+using System.Linq;
 
 class Polynom
 {
     public static int[] PolynomMultiplier(int[] first_polynom, int[] second_polynom)
     {
-        if(first_polynom.Length = 0 || second_polynom.Length = 0)
-        {
-            return;
-        }
-
-        int[] polynom_product = new int[first_polynom.Length + second_polynom.Lenth - 1];
+        int[] polynomProduct = new int[first_polynom.Length + second_polynom.Length - 1];
 
         for(int i = 0; i < first_polynom.Length; i++)
         {
             for(int j = 0; j < second_polynom.Length; j++)
             {
-                polynom_product[i + j] = first_polynom[i] + second_polynom[j];
+                polynomProduct[i + j] += first_polynom[i] * second_polynom[j];
             }
         }
 
-        return polynom_product;
+        return polynomProduct;
     }
 }
 
+class PolynomMultiplierTests
+{
+    public static void Main()
+    {
+    if(BasicTest())
+    {
+        Console.WriteLine("Passed");
+    }
+    else
+    {
+        Console.WriteLine("Failed");
+    }
+    
+    if(!NegativeTest())
+    {
+        Console.WriteLine("Passed");
+    }
+    else
+    {
+        Console.WriteLine("Failed");
+    }
+    
+    if(DifferentLengthTest())
+    {
+        Console.WriteLine("Passed");
+    }
+    else
+    {
+        Console.WriteLine("Failed");
+    }
+    if(BothZeroesTest())
+    {
+        Console.WriteLine("Passed");
+    }
+    else
+    {
+        Console.WriteLine("Failed");
+    }
+    if(OneZeroeTest())
+    {
+        Console.WriteLine("Passed");
+    }
+    else
+    {
+        Console.WriteLine("Failed");
+    }
+    }
+
+    static bool BasicTest()
+    {
+        int[] first_polynom = {-1, 1};
+        int[] second_polynom = {2, 1};
+        int[] expected = {-2, 1, 1};
+        int[] actual = Polynom.PolynomMultiplier(first_polynom, second_polynom);
+        return expected.SequenceEqual(actual);
+    }
+    static bool NegativeTest()
+    {
+        int[] first_polynom = {-1, 1};
+        int[] second_polynom = {2, 1};
+        int[] expected = {-2, 2, 2};
+        int[] actual = Polynom.PolynomMultiplier(first_polynom, second_polynom);
+        return expected.SequenceEqual(actual);
+    }
+
+    static bool DifferentLengthTest()
+    {
+        int[] first_polynom = {1, 2, 9, 3};
+        int[] second_polynom = {2, 7};
+        int[] expected = {2, 11, 32, 69, 21};
+        int[] actual = Polynom.PolynomMultiplier(first_polynom, second_polynom);
+        return expected.SequenceEqual(actual);
+    }
+    static bool BothZeroesTest()
+    {
+        int[] first_polynom = {0};
+        int[] second_polynom = {0};
+        int[] expected = {0};
+        int[] actual = Polynom.PolynomMultiplier(first_polynom, second_polynom);
+        return expected.SequenceEqual(actual);
+    }
+    static bool OneZeroeTest()
+    {
+        int[] first_polynom = {0};
+        int[] second_polynom = {0};
+        int[] expected = {0};
+        int[] actual = Polynom.PolynomMultiplier(first_polynom, second_polynom);
+        return expected.SequenceEqual(actual);
+    }
+}
