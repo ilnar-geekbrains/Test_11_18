030407c844710b9fd146959f1936be5b1ee1e00c
Dmitrii.Valiaev <d.valyaev15@gmail.com>
2022-06-23 18:49:12 +0300

    branch/test_tasks_dmitrii_valiaev add implementation for multiplyArrays method - SecondProblem.java

--- src/main/java/ru/spb/SecondProblem.java
+++ src/main/java/ru/spb/SecondProblem.java
@@ -2,7 +2,21 @@ package ru.spb;
 
 public class SecondProblem {
 
-    public static int[] multiplyArrays(int[] firstArray, int[] secondArray){
-        return null;
+    public static int[] multiplyArrays(int[] firstArray, int[] secondArray) {
+        if (firstArray == null || secondArray == null) {
+            throw new RuntimeException("One of passed arrays is null");
+        }
+        if (firstArray.length == 0 || secondArray.length == 0) {
+            throw new RuntimeException("One of passed arrays is empty");
+        }
+
+        var result = new int[firstArray.length + secondArray.length - 1];
+        for (int i = 0; i < firstArray.length; i++) {
+            for (int j = 0; j < secondArray.length; j++) {
+                result[i + j] += firstArray[i] * secondArray[j];
+            }
+        }
+
+        return result;
     }
 }
