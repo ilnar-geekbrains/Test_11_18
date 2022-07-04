1d755b929803d0c1fb51808d13c8873e6d5509b5
Dmitry Tikhonov <tdmv1209@gmail.com>
2022-07-04 21:41:34 +0300

    Created a method that determines coefficients a power equation

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿int[] GetCoeffsEquation (int[] firstArray, int[] secondArray)
+{
+    int[] result = new int[firstArray.Length + secondArray.Length - 1];
+
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        for (int j = 0; j < secondArray.Length; j++)
+        {
+            int index = i + j;
+            result[index] += firstArray[i] * secondArray[j];
+        }
+    }
+    return result;
+}
