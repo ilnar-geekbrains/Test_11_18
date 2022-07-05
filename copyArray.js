574d61e735b2e2246636eeb6909537420db85c27
Vilmir Baikov <noname777228@rambler.ru>
2022-07-05 17:31:51 +0300

    deepCloneVariable function: array processing

--- copyArray.js
+++ copyArray.js
@@ -16,5 +16,16 @@ const deepCloneVariable = variable => {
             // Собственно тут и происходит рекурсия
             // Углубление в объект идет до тех пор, пока  typeof variable[key] === "object"
     );
-    // Дальше нужна проверка для массива и возврат surfaceСlone
+    // Проверяем является ли variable массивом и не пустой ли он
+    return Array.isArray(variable) && variable.length
+        // Если variable массив и он не пуст, тогда
+        // указываем объекту surfaceСlone длину исходного массива что бы
+        // конвертировать surfaceСlone в массив и вернуть его
+        ? (surfaceСlone.length = variable.length) && Array.from(surfaceСlone)
+        // Если variable пустой массив,
+        : Array.isArray(variable)
+            // то возвращаем его
+            ? Array.from(variable)
+            // в других случаях variable это объект и мы возвращаем копию surfaceСlone.
+            : surfaceСlone;
 };
