6d6dda374008db216971ffe2b5b73c3f1a802d8f
Eugene Elantsev <eugene_el@mail.ru>
2022-07-06 21:31:31 +0700

    add copy function

--- test_tasks/task_001/Program.cs
+++ test_tasks/task_001/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу для копирования массива
+
+int[] copyArray(int[] initArray)
+{
+    int[] resultArray = new int[initArray.GetLength(0)];
+    for (int i = 0; i < initArray.GetLength(0); i++)
+    {
+        resultArray[i] = initArray[i];
+    }
+    return resultArray;
+}
