6447c2f0aec00be31986ae9f8a0bc974fe5f47fe
Eugene Elantsev <eugene_el@mail.ru>
2022-07-06 21:44:12 +0700

    add comments

--- test_tasks/task_001/Program.cs
+++ test_tasks/task_001/Program.cs
@@ -2,10 +2,16 @@
 
 int[] copyArray(int[] initArray)
 {
+    // Объявляем массив для создания копии такого же размера что и исходный массив
     int[] resultArray = new int[initArray.GetLength(0)];
-    for (int i = 0; i < initArray.GetLength(0); i++)
+    // Для всех элементов массива результатов присваиваем 
+    // соответствующее значение исходного массива
+    for (int i = 0; i < resultArray.GetLength(0); i++)
     {
         resultArray[i] = initArray[i];
     }
+    // Функция возвращает резултатом работы копию исходного массива
     return resultArray;
 }
+
+System.Console.WriteLine(string.Join(" ", copyArray(new int[]{1, 2, 3})));
