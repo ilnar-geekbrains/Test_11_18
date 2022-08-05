a4d3b39bef0ca2e3f5a7d467eeaee76535132750
samodurOFF <samodurovdanil@gmail.com>
2022-08-05 18:19:16 +0500

    The test_length method of the TestArrayCopy class, inherited from the TestCase class of the unittest library was created to check the length of the copied array

---
+++ task_1/task_1_test.py
@@ -0,0 +1,17 @@
+from unittest import TestCase, main
+from task_1 import *
+
+
+class TestArrayCopy(TestCase):
+
+    def test_length(self):
+        array = [-1, 1]
+        expected = 2
+        self.assertTrue(len(copy_by_list_method(array)), expected)
+        self.assertTrue(len(copy_by_slice(array)), expected)
+        self.assertTrue(len(copy_by_comprehension(array)), expected)
+        self.assertTrue(len(copy_by_map(array)), expected)
+
+
+if __name__ == '__main__':
+    main()
