eaa492c7296bb2d9dcfcff1dad3a445e12546609
samodurOFF <samodurovdanil@gmail.com>
2022-08-05 18:24:20 +0500

    The test_for_tree_arrays method was created into the TestArrayMultiplication class to test the multiplication of three arrays

--- task_2/task_2_test.py
+++ task_2/task_2_test.py
@@ -12,6 +12,15 @@ class TestArrayMultiplication(TestCase):
         expected = [-2, 1, 1]
         self.assertEqual(main_func(*arrays), expected)
 
+    def test_for_three_arrays(self):
+        arrays = (
+            [-1, 1],
+            [2, 1],
+            [3, 1],
+        )
+        expected = [-6, 1, 4, 1]
+        self.assertEqual(main_func(*arrays), expected)
+
 
 if __name__ == '__main__':
     main()
