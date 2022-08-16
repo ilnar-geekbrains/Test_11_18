5a326991621684a8e9d99615065ee175e97cf9c7
InfinityIn <smailmc68@gmail.com>
2022-08-16 16:17:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pln1 = new int[] { 2, 1};
+            int[] pln2 = new int[] { 2, 4, 3 };
+            foreach(int item in NewPolynom(pln1, pln2))
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+
+        public static int[] NewPolynom(int[] pln1, int[] pln2)
+        {
+            if ((pln1 == null) || (pln2 == null))
+                return new int[0];
+            int[] resPln = new int[Math.Max(pln1.Length, pln2.Length) + 1];
+            for (int i = 0; i < pln1.Length; i++)
+            {
+                for (int j = 0; j < pln2.Length; j++)
+                {
+                    resPln[i+j] += pln1[i] * pln2[j];
+                }    
+            }
+            return resPln;
+        }
+    }
+}
