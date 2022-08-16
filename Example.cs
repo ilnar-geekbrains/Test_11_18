96075c1640dd8341258b4e02df15dd190780b86e
STONE17th <17th@inbox.ru>
2022-08-16 16:13:59 +0300

    Решение тестовых задач

--- Example.cs
+++ Example.cs
@@ -1,50 +1,59 @@
-﻿// Написать программу, которая выводит на экран квадрат заданного числа
+﻿Console.Clear();
+Console.WriteLine("Написать программу копирования массива");
+Console.Write("Введите данные массива (размер, минимальное и макисмальное значение, через пробел): ");
+string SettingArr =  Console.ReadLine() ?? "0";
+string[] SetArr = SettingArr.Split(" ");
+int[] array = new int[Convert.ToInt32(SetArr[0])];
+int[] newArray = new int[array.Length];
 
-// вариант 1. Решение для самых маленьких
-
-int number = 2;
-
-System.Console.WriteLine(number * number);
-
-// Вариант 2. Решение с базовыми элементами тестирования для тех кто постарше.
-
-int GetSquareOfValue(int number)
+Console.WriteLine("Массив: ");
+for (int i=0; i<Convert.ToInt32(SetArr[0]); i++)
 {
-    return number * number;
+    array[i] = new Random().Next(Convert.ToInt32(SetArr[1]),Convert.ToInt32(SetArr[2]));
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine("");
+
+for (int i=0; i<array.Length; i++)
+    newArray[i] = array[i];
+
+Console.WriteLine("Новый массив: ");
+foreach (int k in newArray)
+    Console.Write($"{k} ");
+
+
+// по факту копирование массива можно заложить сразу в первый цикл for
+// где мы присваиваем значения элементам первого массива
+// (после 14ой строки добавить newArray[i] = array[i];)
+// второй и третий циклы добавлены для наглядностви вывода в консоль сначала первого массива, затем нового
+
+
+Console.Write("\n\nНажмите любую клавишу для продолжения...");
+Console.ReadKey();
+
+Console.Clear();
+Console.WriteLine("Написать программу для операции 'произведение массива'");
+Console.Write("Введите элементы первого массива через пробел): ");
+string firstString =  Console.ReadLine() ?? "0";
+Console.Write("Введите элементы второго массива через пробел): ");
+string secondString =  Console.ReadLine() ?? "0";
+
+string[] firstArray = firstString.Split(" ");
+string[] secondArray = secondString.Split(" ");
+int[] firstArr = new int[firstArray.Length];
+int[] secondArr = new int[secondArray.Length];
+int[] finalArr = new int[firstArray.Length + 1];
+
+for (int n=0; n<firstArray.Length; n++)
+{
+    firstArr[n] = Convert.ToInt32(firstArray[n]);
+    secondArr[n] = Convert.ToInt32(secondArray[n]);
 }
 
-if (4 == GetSquareOfValue(2))
-{
-    System.Console.WriteLine("Тест пройден");
-}
-else
-{
-    System.Console.WriteLine("Тест провален");
-}
+for (int n=0; n<firstArray.Length; n++)
+    for (int m=0; m<firstArray.Length; m++)
+        finalArr[m+n] += firstArr[n]*secondArr[m];
 
-if (25 == GetSquareOfValue(-5))
-{
-    System.Console.WriteLine("Тест пройден");
-}
-else
-{
-    System.Console.WriteLine("Тест провален");
-}
-
-if (0 == GetSquareOfValue(0))
-{
-    System.Console.WriteLine("Тест пройден");
-}
-else
-{
-    System.Console.WriteLine("Тест провален");
-}
-
-if (9 != GetSquareOfValue(4))
-{
-    System.Console.WriteLine("Тест пройден");
-}
-else
-{
-    System.Console.WriteLine("Тест провален");
-}
+Console.WriteLine("Произведение массивов: ");
+foreach (int k in finalArr)
+    Console.Write($"{k} ");
