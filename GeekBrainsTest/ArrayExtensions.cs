61398752cf8cc264f253d2fcaaaa6582cf6e54c3
EkateerinaKireeva <ekateerinakireeva@yandex.ru>
2022-08-18 09:50:51 +0300

    Добавление проекта с заданиями

---
+++ GeekBrainsTest/ArrayExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace GeekBrainsTest
+{
+    public static class ArrayExtensions
+    {
+
+        public static T[] DeepClone<T>(this T[] value)
+        {
+            if (value is null)
+                return null;
+
+            return value.Select(s => s).ToArray();
+        }
+
+        public static bool IsEqualsByValue<T>(this T[] source, T[] compareToItem)
+            where T : struct
+        {
+            if (source == null && compareToItem == null)
+                return true;
+
+            if (source == null || compareToItem == null)
+                return false;
+
+            if (source.Length != compareToItem.Length)
+                return false;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (!Equals(source[i], compareToItem[i]))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ GeekBrainsTest/PolynomialMultiplicationOperation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace GeekBrainsTest
+{
+    public class PolynomialMultiplicationOperation
+    {
+        public double[] Multiply(double[] source, double[] multiplierPolynom)
+        {
+            if(source== null)
+                throw new ArgumentNullException(nameof(source));
+
+           if (multiplierPolynom == null)
+                throw new ArgumentNullException(nameof(multiplierPolynom));
+
+            double[] largePolynom;
+            double[] smallPolynom;
+
+            if (source.Length > multiplierPolynom.Length)
+            {
+                largePolynom = source;
+                smallPolynom = multiplierPolynom;
+            }
+            else
+            {
+                largePolynom = multiplierPolynom;
+                smallPolynom = source;
+            }
+
+            int newPlynomLenght = multiplierPolynom.Length + source.Length - 1;
+
+            double[] newPlynom = new double[newPlynomLenght];
+            for (int degree = 0; degree < newPlynomLenght; degree++)
+            {
+                var index  = newPlynomLenght - degree - 1;
+                newPlynom[degree] = GetCoef(degree, largePolynom, smallPolynom);
+            }
+
+            return newPlynom;
+        }
+
+        public double GetCoef(int degree, double[] largePolynom, double[] smallPolynom)
+        {
+            double sum = 0;
+
+            for (int i = 0; i <= Math.Min(degree, smallPolynom.Length -1); i++)
+            {
+                int multIndex = degree - i;
+
+                if (multIndex >= largePolynom.Length)
+                    continue;
+
+                sum += smallPolynom[i] * largePolynom[multIndex];
+            }
+
+            return sum;
+        }
+    }
+}
