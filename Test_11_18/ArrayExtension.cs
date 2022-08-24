91c8d709fea60dea6cf16b4ad45fb2ce93feebe6
Asriel <fr2500@protonmail.com>
2022-08-24 15:13:33 +0300

    feat: Added project with excercises

---
+++ Test_11_18/ArrayExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace Test_11_18;
+
+public static class ArrayExtension
+{
+    public static T[] MakeCopy<T>(this T[] array)
+    {
+        ArgumentNullException.ThrowIfNull(array);
+
+        var copy = new T[array.Length];
+        array.CopyTo(copy, 0);
+        return copy;
+    }
+}
---
+++ Test_11_18/ArrayHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace Test_11_18;
+
+public class ArrayHelper
+{
+    public static double[] MultiplyPolynomials(double[] a, double[] b)
+    {
+        ArgumentNullException.ThrowIfNull(a);
+        ArgumentNullException.ThrowIfNull(b);
+
+        double[] result = new double[a.Length + b.Length - 1];
+
+        Parallel.For(0, a.Length, (i) =>
+        {
+            Parallel.For(0, b.Length, (j) =>
+            {
+                result[i + j] += a[i] * b[j];
+            });
+        });
+
+        return result;
+    }
+}
