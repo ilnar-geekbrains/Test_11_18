3cb825e2e2cd15e1b3e227e1a61359f8927496b8
Asriel <fr2500@protonmail.com>
2022-08-24 20:48:02 +0300

    fix: Replaced "Parallel.For" with regular for to avoid concurrency problems

--- Test_11_18/ArrayHelper.cs
+++ Test_11_18/ArrayHelper.cs
@@ -9,13 +9,9 @@ public class ArrayHelper
 
         double[] result = new double[a.Length + b.Length - 1];
 
-        Parallel.For(0, a.Length, (i) =>
-        {
-            Parallel.For(0, b.Length, (j) =>
-            {
+        for (int i = 0; i < a.Length; i++)
+            for (int j = 0; j < b.Length; j++)
                 result[i + j] += a[i] * b[j];
-            });
-        });
 
         return result;
     }
