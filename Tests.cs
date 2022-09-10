b8451bcc07fba0d4db1c8af77f691ce5e41f0407
Stupenkov Anton <stupean@gmail.com>
2022-09-10 14:27:48 +0600

    рефакториг: вынести дублирующий код в метод PrintStart

--- Tests.cs
+++ Tests.cs
@@ -4,8 +4,9 @@ public static class Tests
 {
     public static void Length_of_arrays_must_be_same(int[] source, int[] destination)
     {
-        Console.WriteLine(
-            $"Тест проверки одинаковой длины массива. Входные данные: source.Length {source.Length} | destination.Length {destination.Length}");
+        PrintStart(
+            "Проверка одинаковой длины массива",
+            $"source.Length {source.Length} | destination.Length {destination.Length}");
 
         bool result = ArrayUtil.Copy(source, destination);
         PrintResult(result);
@@ -13,8 +14,9 @@ public static class Tests
 
     public static void Input_data_must_be_initialized(int[] source, int[] destination)
     {
-        Console.WriteLine(
-            $"Тест проверки на инициализацию массивов. Входные данные: source is initialized - {source != null} | destination is initialized - {destination != null}");
+        PrintStart(
+            "Проверка на инициализацию массивов",
+            $"source is initialized - {source != null} | destination is initialized - {destination != null}");
 
         bool result = ArrayUtil.Copy(source, destination);
         PrintResult(result);
@@ -22,8 +24,9 @@ public static class Tests
 
     public static void Arrays_must_have_the_same_contents(int[] firstArray, int[] secondArray)
     {
-        Console.WriteLine(
-            $"Тест проверки на идентичность массивов. Входные данные: first array - {string.Join(',', firstArray)} | second array  - {string.Join(',', secondArray)}");
+        PrintStart(
+            "Проверка на идентичность массивов",
+            $"first array - {string.Join(',', firstArray)} | second array  - {string.Join(',', secondArray)}");
 
         bool result = true;
         if (firstArray.Length != secondArray.Length)
@@ -43,6 +46,11 @@ public static class Tests
         }
 
         PrintResult(result);
+    }
+
+    private static void PrintStart(string testName, string inputData)
+    {
+        Console.WriteLine($"Тест. {testName}. Входные данные: {inputData}");
     }
 
     private static void PrintResult(bool result)
